0fa4cb95225a5b80e937c7f80b5d46bf6019a918
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2024-02-19 16:23:07 +0900

    some cache

--- CompleteSolution/settings.py
+++ CompleteSolution/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/ubuntu/OikosCompleteSolution/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
