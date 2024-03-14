00c44c72a5bb5341a21f15f37e3d265bd8720198
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2024-03-14 16:24:54 +0900

    Update views.py

--- inbody/views.py
+++ inbody/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render
-from django.contrib import messages
 from .models import Student, InBodyRecord
 
 def inbody(request):
     uid = request.session.get('uid', None)
-    print("UID:", uid)
 
     if not uid:
-        messages.error(request, 'User information not found. Please log in.')
-        return render(request, 'inbody.html', {'student_data': None, 'most_recent_record': None, 'difference': None})
+        message = 'User information not found. Please log in.'
+        return render(request, 'inbody.html', {'message': message, 'student_data': None, 'most_recent_record': None, 'difference': None})
 
     student_data = Student.objects.filter(uid=str(uid)).values().first()
-    print("Student Data:", student_data)
 
     if student_data is None:
-        messages.error(request, 'Student information not found.')
-        return render(request, 'inbody.html', {'student_data': None, 'most_recent_record': None, 'difference': None})
+        message = 'Student information not found.'
+        return render(request, 'inbody.html', {'message': message, 'student_data': None, 'most_recent_record': None, 'difference': None})
 
     most_recent_record = InBodyRecord.objects.filter(uid=str(uid)).order_by('-timestamp').first()
     second_recent_record = InBodyRecord.objects.filter(uid=str(uid)).order_by('-timestamp').exclude(record_id=most_recent_record.record_id).first()
@@ -34,14 +31,14 @@ def inbody(request):
             else:
                 difference[field] = '(차이가 없습니다)'
 
+    if most_recent_record is None or difference is None:
+        message = '측정된 기록이 없습니다.'
+
     context = {
+        'message': message,
         'student_data': student_data,
         'most_recent_record': most_recent_record,
         'difference': difference,
     }
 
-    if most_recent_record is None or difference is None:
-        messages.info(request, '측정된 기록이 없습니다.')
-
-    print("Messages:", messages.get_messages(request))
     return render(request, 'inbody.html', context)
