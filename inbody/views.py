7b3a0ee343b70ef757312f31575ca73fec3123a1
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2024-03-14 16:43:03 +0900

    Update views.py

--- inbody/views.py
+++ inbody/views.py
@@ -1,29 +1,20 @@
 from django.shortcuts import render
-from django.contrib import messages
-from datetime import datetime, timedelta
-from .models import Student, GameRecord, InBodyRecord
+from .models import Student, InBodyRecord
 
 def inbody(request):
-    uid = request.session.get('uid', None)
+    uid = request.session.get('uid')
 
-    if not uid:
-        messages.error(request, 'User information not found. Please log in.')
+    if uid is None:
         return render(request, 'inbody.html', {'student_data': None, 'most_recent_record': None, 'difference': None})
 
-    # Query the database using uid to get user-specific data
-    student_data = Student.objects.filter(uid=str(uid)).values().first()
+    student_data = Student.objects.filter(uid=str(uid)).first()
 
     if student_data is None:
-        messages.error(request, 'Student information not found.')
         return render(request, 'inbody.html', {'student_data': None, 'most_recent_record': None, 'difference': None})
 
-    # Fetch the most recent inbody record for the user
     most_recent_record = InBodyRecord.objects.filter(uid=str(uid)).order_by('-timestamp').first()
-
-    # Fetch the second most recent inbody record for the user
     second_recent_record = InBodyRecord.objects.filter(uid=str(uid)).order_by('-timestamp').exclude(record_id=most_recent_record.record_id).first()
 
-    # Calculate the difference between most recent and second most recent data
     difference = None
     if most_recent_record and second_recent_record:
         difference = {}
@@ -38,12 +29,4 @@ def inbody(request):
             else:
                 difference[field] = '(차이가 없습니다)'
 
-
-    # Pass the data to the template
-    context = {
-        'student_data': student_data,
-        'most_recent_record': most_recent_record,
-        'difference': difference,
-    }
-
-    return render(request, 'inbody.html', context)
+    return render(request, 'inbody.html', {'student_data': student_data, 'most_recent_record': most_recent_record, 'difference': difference})
