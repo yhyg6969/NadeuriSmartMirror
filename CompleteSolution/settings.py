522e9a06a0f8bb78ee2a05964de60db423c64097
yhyg6969 <152124667+yhyg6969@users.noreply.github.com>
2024-05-31 09:25:04 +0900

    Update settings.py

--- CompleteSolution/settings.py
+++ CompleteSolution/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
-#STATIC_ROOT = '/home/ubuntu/OikosCompleteSolution/static/'
+STATIC_ROOT = '/home/ubuntu/OikosCompleteSolution/static/'
 
 
 # Default primary key field type
