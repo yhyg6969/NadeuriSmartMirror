bb45fe3606b162d01da4eeeff0861c93095a790a
yhyg6969 <152124667+yhyg6969@users.noreply.github.com>
2024-07-24 12:46:30 +0900

    add center

--- smartmirror/views.py
+++ smartmirror/views.py
@@ -34,6 +34,7 @@ def smartmirror(request):
                 "도봉구 치매안심센터": "0000",
                 "구로구 치매안심센터": "0000",
                 "양천구 치매안심센터": "0000",
+                "용산구 치매안심센터": "0000",
             }
             
             if center_name not in center_passwords or password != center_passwords[center_name]:
