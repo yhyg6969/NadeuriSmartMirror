71f182cdae7eedf51382e9174531058f3214d32a
yhyg6969 <152124667+yhyg6969@users.noreply.github.com>
2025-01-15 15:03:23 +0900

    Update views.py

--- smartmirror/views.py
+++ smartmirror/views.py
@@ -56,6 +56,7 @@ def smartmirror(request):
                 "도봉노인종합복지관": "0000",
                 "금천노인종합복지관": "0000",
                 "성동노인종합복지관": "0000",
+                "서울노인복지센터": "0000",
             }
             
             if center_name not in center_passwords or password != center_passwords[center_name]:
